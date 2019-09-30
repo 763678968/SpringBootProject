4d12d06e5538ab4216f639c9cbbfa6c607bde10e
animation <763678968@qq.com>
2019-09-30 21:35:32 +0800

    自动配置

--- src/main/java/com/example/springbootproject/controller/BootController.java
+++ src/main/java/com/example/springbootproject/controller/BootController.java
@@ -1,8 +1,13 @@
 package com.example.springbootproject.controller;
 
+import com.example.springbootproject.entity.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class BootController {
@@ -10,5 +15,20 @@ public class BootController {
     @RequestMapping("/request")
     public String request() {
         return "hello spring boot!";
+    }
+
+    @RequestMapping("/welcome")
+    public String welcome(Map<String, Object> map) {
+        // 给thymeleaf准备数据
+        map.put("welcome", "welcomeThymeleaf"); // 给request域中放welcome
+
+        List<Product> prods = new ArrayList<>();
+        prods.add(new Product("a", 100, 10));
+        prods.add(new Product("b", 200, 20));
+        prods.add(new Product("c", 300, 30));
+
+        map.put("prods", prods);
+
+        return "result";
     }
 }
---
+++ src/main/java/com/example/springbootproject/entity/Product.java
@@ -0,0 +1,40 @@
+package com.example.springbootproject.entity;
+
+public class Product {
+    private String name;
+    private double price;
+    private int inStock;
+
+    public Product() {
+    }
+
+    public Product(String name, double price, int inStock) {
+        this.name = name;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getInStock() {
+        return inStock;
+    }
+
+    public void setInStock(int inStock) {
+        this.inStock = inStock;
+    }
+}
